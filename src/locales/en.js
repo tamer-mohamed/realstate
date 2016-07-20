95aa22365ca38bdaa9ca6a9750094f62cd2647c4
pyrozoa <pyrozoa@gmail.com>
2016-07-20 14:47:28 +0300

    translations

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,9 +1,260 @@
 module.exports = {
-  locale: "en-AU",
+  locale: "en",
+  formats: {},
   className: "ltr",
-  messages: {
-    "helloWorld": "Hello World",
-    "menu.home": "home",
-    "home": "home"
-  }
+    "pluralRuleFunction": function(){
+    },
+messages: {
+    "settings.currency": "KWD",
+    
+    "contextualMenu.addProperty": "Add Property ",
+    
+    "screen.about": "About Us",
+    "screen.breadcrumb": "About Us",
+    
+    // main menu
+    "menu.home": "Home",
+    "menu.about": "About Us",
+    "menu.contact": "Contact Us",
+    
+    // user menu
+    "userMenu.signout": "Log Out",
+    "userMenu.allProperties": "My Propertie",
+    "userMenu.contact": "My Profile",
+    
+    // Contextual Menu
+    "contextualMenu.user.addProperty": "Add Property",
+    
+    // filters
+    "filters.submitButton": "Submit",
+    "filters.locationField": "Location",
+    "filters.typeField": "Type",
+    "filters.purposeField": "Purpose",
+    
+    "block.submitProperty.text": "Would you like to add a property?",
+    "block.submitProperty.buttonText": "Add Property",
+    
+    
+    // validations
+    "forms.validations.correctErrors": "ERROR: Please Correct the errors below!",
+    "forms.validations.generic.required": "This Field is required!",
+    "forms.validations.generic.isNumeric": "This Field Accepts Numbers Only!",
+    "forms.validations.user.passwordMatch": "Password Mis-Match",
+    
+    // registration form
+    "screen.user.register.pageTitle": "Register",
+    "forms.user.register.errors.addUser": "Error in adding User",
+    "forms.user.register.fields.email": "E-Mail ",
+    "forms.user.register.fields.firstName": "Name",
+    "forms.user.register.fields.password": "Password",
+    "forms.user.register.fields.repeatPassword": "Repeat Password",
+    "forms.user.register.submit": "Register",
+    "forms.user.register.fields.accountType": "Account type",
+    "forms.user.register.success": "Registration is Successful",
+    
+    
+    //
+    "screen.secure.properties.listing.propertyTitle": "Title",
+    "screen.secure.properties.listing.propertyFeatureLevel": "",
+    "screen.secure.properties.listing.manageProperty": " ",
+    
+    // generic form vars
+    "forms.generic.add": "Add",
+    "forms.generic.select": "-- ¬Select --",
+    "forms.generic.update": "Update",
+    
+    //login form
+    "forms.users.login.submit": "Log In",
+    
+    
+    // user all properties
+    "screen.secure.properties.pageTitle": "Properties",
+    
+    
+    // add property
+    "screen.secure.properties.add.pageTitle": "Add Property",
+    "screen.secure.properties.manage.pageTitle": "Edit Property",
+    "forms.property.add.labels.genericInfo": "Additional Information Regarding your Property",
+    "forms.property.add.labels.address": "Location Information",
+    "forms.property.add.labels.featuredLevel": "Make your Listing Stand Out",
+    "forms.property.add.labels.info": "Information",
+    "forms.property.success": "Listing is Successful",
+    
+    "forms.property.add.fields.title": "Title",
+    "forms.property.add.fields.price": "Price",
+    "forms.property.add.fields.space": "Space",
+    "forms.property.add.fields.purpose": "Purpose",
+    "forms.property.add.fields.type": "Type",
+    "forms.property.add.fields.location": "Governate",
+    "forms.property.add.fields.area": "Area",
+    
+    
+    // forms error
+    "forms.errors.property.add": "Error: Can't Add Property!",
+    
+    // prefrences
+    "property.preference.beds": "Beds",
+    "property.preference.rooms": "Rooms",
+    "property.preference.kitchen": "Kitchens",
+    "property.preference.bathroom": "Toilets",
+    "property.preference.apartment": "Appartment",
+    "property.preference.balcony": "Bacony",
+    "property.preference.parking": "Parking",
+    "property.preference.floors": "Floors",
+    "property.preference.swimmingPool": "Swimming Pool",
+    "property.preference.laborResidence": "Labor Residence",
+    "property.preference.villa": "Villa",
+    "property.preference.waterWell": "Water Well",
+    
+    
+    "areas.ahmadi.abuHalifa": "Abu Halifa",
+    "areas.ahmadi.ahmadi": "AL-Ahmadi",
+    "areas.ahmadi.bnaider": "Bnaider",
+    "areas.ahmadi.dhaher": "Dhaher",
+    "areas.ahmadi.eqaila": "Eqaila",
+    "areas.ahmadi.fahadAlAhmed": "Fahad Al Ahmad",
+    "areas.ahmadi.fahaheel": "Fahaheel",
+    "areas.ahmadi.fintas": "Fintas",
+    "areas.ahmadi.hadiya": "Hadiya",
+    "areas.ahmadi.jaberAlAli": "Jaber Al-ali",
+    "areas.ahmadi.julaia": "Julaia",
+    "areas.ahmadi.khairan": "Khairan",
+    "areas.ahmadi.mahboula": "Mahboula",
+    "areas.ahmadi.mangaf": "Mangaf",
+    "areas.ahmadi.minaAbdullah": "Abdullah Sea-Port",
+    "areas.ahmadi.omAlHayman": "Om Al-Hayman",
+    "areas.ahmadi.rigga": "Rigga",
+    "areas.ahmadi.sabahAlAhmadCity": "Sabah Al Ahmad City",
+    "areas.ahmadi.sabahAlAhmadLagoon": "Sabah Al Ahmad Lagoon",
+    "areas.ahmadi.sabahiya": "Sabahiya",
+    "areas.ahmadi.shuaibaNorth": "North Shuaiba",
+    "areas.ahmadi.shuaibaSouth": "Shuaiba South",
+    "areas.ahmadi.wafra": "Warfa",
+    "areas.farwaniya.abdullaAlMubarak": "Abdullah Al-Mubarak",
+    "areas.farwaniya.airportDistrict": "Airport District",
+    "areas.farwaniya.andalous": "Andalous",
+    "areas.farwaniya.ardhiya": "Ardiyah",
+    "areas.farwaniya.ardhiyaIndustrial": "Ardiyah Industrial",
+    "areas.farwaniya.ashbeeliya": "Ishbiliya",
+    "areas.farwaniya.dhajeej": "Dhajeej",
+    "areas.farwaniya.farwaniya": "Farwaniya",
+    "areas.farwaniya.firdous": "Firdous",
+    "areas.farwaniya.jleebAlShiyoukh": "Jleeb Al Shiyoukh",
+    "areas.farwaniya.khaitan": "Khaitan",
+    "areas.farwaniya.omariya": "Omariya",
+    "areas.farwaniya.rabya": "Rabia",
+    "areas.farwaniya.raiIndustrial": "Rai Industrial",
+    "areas.farwaniya.reggae": "AL-Reggaei",
+    "areas.farwaniya.rehab": "Rehab",
+    "areas.farwaniya.sabahAlNasser": "Sabah Al-Nasser",
+    "areas.hawally.bayan": "Bayan",
+    "areas.hawally.bida": "Bida",
+    "areas.hawally.hawally": "Hawalli",
+    "areas.hawally.hitteen": "Hitten",
+    "areas.hawally.jabriya": "Jabriya",
+    "areas.hawally.midanHawalli": "Maidan Hawalli",
+    "areas.hawally.mishref": "Mishref",
+    "areas.hawally.mubarakAlAbdulla": "Mubarak Al Abdullah",
+    "areas.hawally.rumaithiya": "Rumaithiya",
+    "areas.hawally.salam": "Salam",
+    "areas.hawally.salmiya": "Salmiya",
+    "areas.hawally.salwa": "Salwa",
+    "areas.hawally.shaab": "Shaab",
+    "areas.hawally.shaabAlBahri": "Shaab Al Bahri",
+    "areas.hawally.shuhada": "Shuhada",
+    "areas.hawally.sideeq": "Al Sideeq",
+    "areas.hawally.zahra": "Zahra",
+    "areas.jahra.abdali": "Abdali",
+    "areas.jahra.jahra": "Jahra",
+    "areas.jahra.jahraIndustrial": "Jahra Industrial",
+    "areas.jahra.naeem": "Naeem",
+    "areas.jahra.nasseem": "Nassem",
+    "areas.jahra.oyoun": "Oyoun",
+    "areas.jahra.qasr": "Qasr",
+    "areas.jahra.rawdhatain": "Rawdhatain",
+    "areas.jahra.saadAlAbdullah": "Saad Al Abdullah",
+    "areas.jahra.subiya": "Subiya",
+    "areas.jahra.sulaibiya": "Sulaibiya",
+    "areas.jahra.sulaibiyaIndustrial": "Sulaibiya Industrial",
+    "areas.jahra.taima": "Taima",
+    "areas.jahra.waha": "Waha",
+    "areas.jahra.westJahra": " West Jahra",
+    "areas.kuwaitCity.abdullaAlSalem": "Abdulla Al-Salem",
+    "areas.kuwaitCity.adailiya": "Al-Adiliya",
+    "areas.kuwaitCity.alNahda": "Al Nahda",
+    "areas.kuwaitCity.alQairawan": "Al-Qairawan",
+    "areas.kuwaitCity.bneidAlQar": "Bneid Al-Qar",
+    "areas.kuwaitCity.daiya": "Daiya",
+    "areas.kuwaitCity.dasma": "Dasma",
+    "areas.kuwaitCity.dasman": "Dasman",
+    "areas.kuwaitCity.doha": "Doha",
+    "areas.kuwaitCity.faiha": "Faiha",
+    "areas.kuwaitCity.freeTradeZone": "Free Trade Zone",
+    "areas.kuwaitCity.ghornata": "Ghornata",
+    "areas.kuwaitCity.jaberAlAhmad": "Jaber Al-Ahmad ",
+    "areas.kuwaitCity.kaifan": "Kaifan",
+    "areas.kuwaitCity.khaldiya": "Khaldiya",
+    "areas.kuwaitCity.kuwaitCity": "Kuwait City",
+    "areas.kuwaitCity.mansouriya": "Mansouriya",
+    "areas.kuwaitCity.mirqab": "Mirqab",
+    "areas.kuwaitCity.nuzha": "Nuzha",
+    "areas.kuwaitCity.qadsiya": "Qadsiya",
+    "areas.kuwaitCity.qibla": "Qibla",
+    "areas.kuwaitCity.qortuba": "Qortuba",
+    "areas.kuwaitCity.rawda": "Rawda",
+    "areas.kuwaitCity.shamiya": "Shamiya",
+    "areas.kuwaitCity.sharq": "Sharq",
+    "areas.kuwaitCity.shuwaikhIndustrial": "Shuwaikh Industrial",
+    "areas.kuwaitCity.shuwaikhResidential": " Shuwaikh Residential",
+    "areas.kuwaitCity.sulaibikhat Sulaibikhat": "Al-Sulaibikhat",
+    "areas.kuwaitCity.surra": "Surra",
+    "areas.kuwaitCity.yarmouk": "Yarmouk",
+    "areas.mubarakAlKabeer.abuAlHassaniya": "Abu Al-Hassaniya",
+    "areas.mubarakAlKabeer.abuFutaira": "Abu Futaira",
+    "areas.mubarakAlKabeer.adan": "Al Adan",
+    "funaitees": "Funaitees",
+    "areas.mubarakAlKabeer.messila": "Messila",
+    "areas.mubarakAlKabeer.mubarakAlKabeer": "Mubarak Al Kabeer",
+    "areas.mubarakAlKabeer.qosour": "Qosour",
+    "areas.mubarakAlKabeer.qurain": "Al Qurain",
+    "areas.mubarakAlKabeer.sabahAlSalem": "Sabah Al Salem",
+    "areas.mubarakAlKabeer.subhan": "Subhan",
+    "areas.mubarakAlKabeer.wista": "Wista",
+    
+    
+    "locations.ahmadi": "Al-Ahmadi",
+    "locations.farwaniya": "Farwaniya ",
+    "locations.hawally": "Hawalli",
+    "locations.jahra": "Jahra",
+    "locations.kuwaitCity": "Kuwait City",
+    "locations.mubarakAlKabeer": "Mubarak Al Kabeer",
+    
+    
+    "purposes.rent": "Rent",
+    "purposes.sale": "Sale",
+    
+    
+    "types.apartment": "Apartment",
+    "types.beachHouse": "Beach House",
+    "types.buildings": "Building",
+    "types.complex": "Complex",
+    "types.cottage": "Cottage",
+    "types.farm": "Farm",
+    "types.floor": "Floor",
+    "types.industrial": "Industrial Land",
+    "types.land": "Land",
+    "types.office": "Office",
+    "types.residential": "Residential Land",
+    "types.shop": "Store",
+    "types.showroom": "Showroom",
+    "types.villa": "Villa",
+    "types.warehouse": "Warehouse",
+    
+    //TODO: change the following the corresponding featured levels in firebase
+    // ex "featuredLevel.tamam" : "tamam"
+    "featuredLevel.1": "Special",
+    "featuredLevel.2": "Main Slider",
+    "featuredLevel.3": "Weekly Newsletter",
+    "featuredLevel.4": "Direct Mail",
 }
+};
